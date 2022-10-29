73f854db953d1d21c85a2dcaf72b299e47f659cd
masteralex <110934776+alexrinve@users.noreply.github.com>
2022-10-28 22:13:14 -0500

    dashboard

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,10 @@ import Home from './components/Home';
 import { ProductDetails } from './components/products/ProductDetails';
 //Router traido desde react-router-dom (no confundir con el de express)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/admin/Dashboard.js';
+import ProductsList from './components/admin/ProductsList.js';
+import NewProduct from './components/admin/newProduct';
+import Cart from './components/cart/Cart';
 
 
 function App() {
@@ -18,6 +22,10 @@ function App() {
             <Route path="/" element={<Home />}/>
             <Route path="/Home" element={<Home />}/>
             <Route path="/producto/:id" element={<ProductDetails />}/>
+            <Route path="/dashboard" element={<Dashboard />}/>
+            <Route path="/productList" element={<ProductsList />}/>
+            <Route path="/nuevoProducto" element={<NewProduct />}/>
+            <Route path="/carrito" element={<Cart />}/>
           </Routes>
         </div>
         <Footer />
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,5 +1,6 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import "../../App.css"
+import { Link } from "react-router-dom"
 
 const Header = () => {
 
@@ -8,7 +9,7 @@ const Header = () => {
             <nav className='navbar row'>
                 <div className='col-12 col-md-3'>
                     <div className='navbar-brand'>
-                        <img src="./images/icon3.png" alt="Emplas Logo"></img>
+                        <Link to="/" ><img src="./images/icon3.png" alt="Emplas"></img></Link>
                     </div>
                 </div>
 
@@ -27,14 +28,20 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                    <div className="ml-4 dropdown d-inline">
+                        <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button"
+                            id="dropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span>Panel de Control</span></Link>
+                        <div className='dropdown-menu' aria-labelledby='dropDownMenu'>
+                            <Link className="dropdown-item" to="/dashboard">Adm. Productos</Link>
+                            <Link className="dropdown-item" to="/">Pedidos</Link>
+                            <Link className="dropdown-item" to="/">Mi cuenta</Link>
+                            <Link className="dropdown-item" to="/">Cerrar Sesion</Link>
+                        </div>
+                    </div>
 
-                <button type="button" class="btn btn-info">Inicie secion</button>
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                   
-                    <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
-                    &nbsp; &nbsp;
-                    <span className="ml-1" id="cart_count">2</span>
-                    
+                    <Link to="/carrito"><i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
+                        <span className="ml-1" id="cart_count">2</span></Link>
                 </div>
 
             </nav>
@@ -42,6 +49,6 @@ const Header = () => {
         </Fragment>
     )
 
-    }
+}
 
 export default Header
