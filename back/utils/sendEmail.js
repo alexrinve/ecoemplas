e70f51cb207773881b33ba35d647e61f84854946
masteralex <110934776+alexrinve@users.noreply.github.com>
2022-11-07 21:24:24 -0500

    orders

--- back/utils/sendEmail.js
+++ back/utils/sendEmail.js
@@ -2,11 +2,11 @@ const nodemailer= require("nodemailer")
 
 const sendEmail = async options =>{
     const transport = nodemailer.createTransport({
-        host: " smtp.office365.com",
+        host: "smtp.office365.com",
         port: 587,
         auth: {
           user: "alexrinve@hotmail.com",
-          pass: "jelzmzlnkzzuczgy"
+          pass: "truhxouodcueeczo"
         }
       });
     const mensaje={
